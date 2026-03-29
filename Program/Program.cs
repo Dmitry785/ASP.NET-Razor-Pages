496764216bd0a622b8ca5ef161cd7e9555578da1
Vladimir Sviridov <ssViridd@gmail.com>
2026-03-29 16:01:48 +0700

    add razor pages

--- Program/Program.cs
+++ Program/Program.cs
@@ -6,8 +6,10 @@ namespace Program
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-
+            builder.Services.AddRazorPages(options => options.RootDirectory = "/Pages");
             var app = builder.Build();
+
+            app.MapRazorPages();
 
             app.Run();
         }
